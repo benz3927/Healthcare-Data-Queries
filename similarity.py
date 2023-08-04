7d00b36c0ab59ae2153a4bed34e0796a60017a32
benz3927 <benz3927@gmail.com>
2023-08-03 20:02:49 -0400

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -41,28 +41,27 @@ df.to_csv('weighted_similarity_scores.csv', index=False)
 print(df)
 
 
-import pandas as pd
-
-# Read the CSV file into the DataFrame 'df'
-url2 = 'https://raw.githubusercontent.com/benz3927/Healthcare-Data-Queries/main/weighted_similarity_scores.csv'
-df = pd.read_csv(url2)
-
-# Calculate the Weighted Score
-weighted_score = (df['NPI_First_Name_String_Distance_Score'] * df['First_Name_Score']) + \
-                 (df['NPI_Last_Name_String_Distance_Score'] * df['Last_Name_Score']) + \
-                 (df['NPI_Middle_Initial_String_Distance_Score'] * df['Middle_Name_Score']) + \
-                 (df['NPI_Gender_Code_String_Distance_Score'] * df['Gender_Score']) + \
-                 (df['NPI_Address_Line_1_String_Distance_Score'] * df['Address_Line_1_Score']) + \
-                 (df['NPI_City_String_Distance_Score'] * df['City_Score']) + \
-                 (df['NPI_State_String_Distance_Score'] * df['State_Score']) + \
-                 (df['NPI_Zip_Code_String_Distance_Score'] * df['Zip_Score'])
-
-# Add the 'Weighted_Score' column to the DataFrame
-df['Weighted_Score'] = weighted_score
-
-# Export the new DataFrame to a CSV file
-output_csv_url = 'https://raw.githubusercontent.com/benz3927/Healthcare-Data-Queries/main/final_weighted_scores.csv'
-df.to_csv(output_csv_url, index=False)
-
-print("New CSV file 'final_weighted_scores.csv' has been created with the additional 'Weighted_Score' column.")
+# 
+# # Read the CSV file into the DataFrame 'df'
+# url2 = 'https://raw.githubusercontent.com/benz3927/Healthcare-Data-Queries/main/weighted_similarity_scores.csv'
+# df = pd.read_csv(url2)
+# 
+# # Calculate the Weighted Score
+# weighted_score = (df['NPI_First_Name_String_Distance_Score'] * df['First_Name_Score']) + \
+#                  (df['NPI_Last_Name_String_Distance_Score'] * df['Last_Name_Score']) + \
+#                  (df['NPI_Middle_Initial_String_Distance_Score'] * df['Middle_Name_Score']) + \
+#                  (df['NPI_Gender_Code_String_Distance_Score'] * df['Gender_Score']) + \
+#                  (df['NPI_Address_Line_1_String_Distance_Score'] * df['Address_Line_1_Score']) + \
+#                  (df['NPI_City_String_Distance_Score'] * df['City_Score']) + \
+#                  (df['NPI_State_String_Distance_Score'] * df['State_Score']) + \
+#                  (df['NPI_Zip_Code_String_Distance_Score'] * df['Zip_Score'])
+# 
+# # Add the 'Weighted_Score' column to the DataFrame
+# df['Weighted_Score'] = weighted_score
+# 
+# # Export the new DataFrame to a CSV file
+# output_csv_url = 'https://raw.githubusercontent.com/benz3927/Healthcare-Data-Queries/main/final_weighted_scores.csv'
+# df.to_csv(output_csv_url, index=False)
+# 
+# print("New CSV file 'final_weighted_scores.csv' has been created with the additional 'Weighted_Score' column.")
 
