4f963e94d2c31a3e1e49aa29d1f46533860c1b78
benz3927 <benz3927@gmail.com>
2023-08-07 14:36:32 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
-npi_url = 'https://github.com/benz3927/Healthcare-Data-Queries/blob/main/data/npi/npi_weekly.csv'
+npi_path = '/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/npi/npi_weekly.csv'
 
 # Read the first few lines of the CSV file
-npi_df_preview = pd.read_csv(npi_url, sep='\t', nrows=10)
+npi_df_preview = pd.read_csv(npi_path, sep='\t', nrows=10)
 print(npi_df_preview)
 
