a71db2f19cae6d1350f33cf7140db7ebf4683655
benz3927 <benz3927@gmail.com>
2023-08-08 14:14:27 -0400

    Create extract_pecos.sql

---
+++ extract_pecos.sql
@@ -0,0 +1,10 @@
+SELECT
+    frst_nm  AS pecos_fname,
+    lst_nm AS pecos_lname,
+    mid_nm AS pecos_mname,
+    gndr AS pecos_gender,
+    adr_ln_1 AS pecos_adr1,
+    cty AS pecos_city,
+    st AS pecos_State,
+    zip AS pecos_zip
+FROM hcare.pecos
