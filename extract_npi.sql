6a6da8dd586129346a43927f4b4a7cd23a285cd4
benz3927 <benz3927@gmail.com>
2023-08-08 18:03:10 -0400

    Update extract_npi.sql

--- extract_npi.sql
+++ extract_npi.sql
@@ -5,6 +5,6 @@ SELECT
     Provider_Gender_Code AS npi_gender,
     Provider_First_Line_Business_Mailing_Address AS npi_adr1,
     Provider_Business_Mailing_Address_City_Name AS npi_city,
-    Provider_Business_Mailing_Address_State_Name AS NPI_State,
+    Provider_Business_Mailing_Address_State_Name AS npi_state,
     Provider_Business_Mailing_Address_Postal_Code AS npi_zip
 FROM hcare.NPI
