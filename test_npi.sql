1f1fa29a66fb488db1653bdb271e6222e58c3b77
benz3927 <benz3927@gmail.com>
2023-08-08 14:14:30 -0400

    Update test_npi.sql

--- test_npi.sql
+++ test_npi.sql
@@ -1,13 +1,10 @@
-INSERT OVERWRITE LOCAL DIRECTORY '/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/'
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
 SELECT
-    REPLACE(Provider_First_Name, ',', '') AS NPI_First_Name,
-    REPLACE(Provider_Last_Name_Legal_Name, ',', '') AS NPI_Last_Name,
-    Provider_Middle_Name AS NPI_Middle_Initial,
-    Provider_Gender_Code AS NPI_Gender_Code,
-    Provider_First_Line_Business_Mailing_Address AS NPI_Address_Line_1,
-    Provider_Business_Mailing_Address_City_Name AS NPI_City,
+    Provider_First_Name AS npi_fname,
+    Provider_Last_Name_Legal_Name AS npi_lname,
+    Provider_Middle_Name AS npi_mname,
+    Provider_Gender_Code AS npi_gender,
+    Provider_First_Line_Business_Mailing_Address AS npi_adr1,
+    Provider_Business_Mailing_Address_City_Name AS npi_city,
     Provider_Business_Mailing_Address_State_Name AS NPI_State,
-    Provider_Business_Mailing_Address_Postal_Code AS NPI_Zip_Code
+    Provider_Business_Mailing_Address_Postal_Code AS npi_zip
 FROM hcare.NPI
