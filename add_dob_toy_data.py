655263ab0d0bb75b9f19344945e2997e14182447
benz3927 <benz3927@gmail.com>
2023-08-11 16:11:32 -0400

    Update add_dob_toy_data.py

--- add_dob_toy_data.py
+++ add_dob_toy_data.py
@@ -1,30 +1,33 @@
 import pandas as pd
 
-# Load the DOB toy data with the correct delimiter
-dob_data = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/dob_toy_data.csv', delimiter='\t')
+# Load toy data
+toy_data = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/dob_toy_data.csv', delimiter='\t', dtype=str)
 
-# Split the combined column into separate columns
-dob_data[['npi', 'nppes_dob', 'pecos_dob']] = dob_data['NPI,NPPES_DOB,PECOS_DOB'].str.split('\t', expand=True)
+# Split the combined column name and assign correct column names
+toy_data[['NPI', 'NPPES_DOB', 'PECOS_DOB']] = toy_data['NPI,NPPES_DOB,PECOS_DOB'].str.split(',', expand=True)
 
-# Define the columns to check for matching NPI values
-columns_to_check = ['npi', 'nppes_npi', 'pecos_npi']
+# Load npi.csv
+npi_data = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/npi_cleaned.csv', dtype=str)
 
-# Load npi_cleaned.csv
-npi_data = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/npi_cleaned.csv')
+# Merge toy_data with npi_data based on 'NPI'
+npi_merged = npi_data.merge(toy_data[['NPI', 'NPPES_DOB']], how='left', left_on='nppes_npi', right_on='NPI')
 
-# Load pecos_cleaned.csv
-pecos_data = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/pecos_cleaned.csv')
+# Load pecos.csv
+pecos_data = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/pecos_cleaned.csv', dtype=str)
 
-# Iterate through the columns and merge DOB data
-for column in columns_to_check:
-    dob_data[column] = dob_data[column].astype(str).str.upper()  # Convert NPI values to uppercase
-    npi_data = pd.merge(npi_data, dob_data[[column, 'nppes_dob']], how='left', left_on='nppes_npi', right_on=column)
-    pecos_data = pd.merge(pecos_data, dob_data[[column, 'pecos_dob']], how='left', left_on='pecos_npi', right_on=column)
+# Merge toy_data with pecos_data based on 'NPI'
+pecos_merged = pecos_data.merge(toy_data[['NPI', 'PECOS_DOB']], how='left', left_on='pecos_npi', right_on='NPI')
 
-# Save the updated npi_cleaned.csv file
-npi_data.to_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/npi_cleaned.csv', index=False)
+# Drop the extra 'NPI' columns from npi_merged and pecos_merged
+npi_merged.drop(columns=['NPI'], inplace=True)
+pecos_merged.drop(columns=['NPI'], inplace=True)
 
-# Save the updated pecos_cleaned.csv file
-pecos_data.to_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/pecos_cleaned.csv', index=False)
+# Convert column names to lowercase
+npi_merged.columns = npi_merged.columns.str.lower()
+pecos_merged.columns = pecos_merged.columns.str.lower()
 
-print("DOB data added to NPI and PECOS files.")
+# Save the updated npi.csv and pecos.csv files
+npi_merged.to_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/npi_with_dob.csv', index=False)
+pecos_merged.to_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/pecos_with_dob.csv', index=False)
+
+print("DOB column added to npi.csv and pecos.csv.")
