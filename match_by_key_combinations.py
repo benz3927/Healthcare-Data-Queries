9ad8494ef4357d44a081042849ba8fb9d0c61081
benz3927 <benz3927@gmail.com>
2023-08-16 10:48:49 -0400

    Update match_by_key_combinations.py

--- match_by_key_combinations.py
+++ match_by_key_combinations.py
@@ -8,16 +8,6 @@ npi_with_dob = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Quer
 # Load pecos_with_dob.csv
 pecos_with_dob = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/pecos_sample.csv', dtype=str)
 
-# Perform a cross join to generate all possible combinations
-combined_data = pd.merge(npi_with_dob.assign(key=1), pecos_with_dob.assign(key=1), on='key', how='outer').drop('key', axis=1)
-
-# Initialize a list to store the candidates
-candidates = []
-
-columns_to_compare = [
-    'fname', 'lname', 'mname', 'gender', 'adr1', 'city', 'state', 'zip', 'phone', 'dob'
-]
-
 # Function to calculate similarity score
 def calculate_similarity_score(str1, str2):
     similarity_score = 1 - dl.normalized_damerau_levenshtein_distance(str1, str2)
@@ -28,40 +18,83 @@ def sanitize_string(s):
     s = re.sub(r'[^a-zA-Z0-9]', '', str(s))  # Remove special characters
     return s
 
-# Iterate through each row of the combined dataset
-for index, row in combined_data.iterrows():
-    npi_row = row[npi_columns]  # Extract NPI row
-    pecos_row = row[pecos_columns]  # Extract PECOS row
+# Add key columns to NPI and PECOS DataFrames
+npi_with_dob['npi_key1'] = (
+    npi_with_dob['npi_dob'] + npi_with_dob['npi_adr1'].apply(sanitize_string)
+)
+npi_with_dob['npi_key2'] = (
+    npi_with_dob['npi_dob'] + npi_with_dob['npi_lname'].apply(sanitize_string)
+)
+npi_with_dob['npi_key3'] = (
+    npi_with_dob['npi_lname'].apply(sanitize_string) + npi_with_dob['npi_adr1'].apply(sanitize_string)
+)
+npi_with_dob['npi_key4'] = (
+    npi_with_dob['npi_lname'].apply(sanitize_string) + npi_with_dob['npi_phone'].apply(sanitize_string)
+)
+
+pecos_with_dob['pecos_key1'] = (
+    pecos_with_dob['pecos_dob'] + pecos_with_dob['pecos_adr1'].apply(sanitize_string)
+)
+pecos_with_dob['pecos_key2'] = (
+    pecos_with_dob['pecos_dob'] + pecos_with_dob['pecos_lname'].apply(sanitize_string)
+)
+pecos_with_dob['pecos_key3'] = (
+    pecos_with_dob['pecos_lname'].apply(sanitize_string) + pecos_with_dob['pecos_adr1'].apply(sanitize_string)
+)
+pecos_with_dob['pecos_key4'] = (
+    pecos_with_dob['pecos_lname'].apply(sanitize_string) + pecos_with_dob['pecos_phone'].apply(sanitize_string)
+)
+
+# Initialize a list to store the candidates
+candidates = []
+
+columns_to_compare = [
+    'fname', 'lname', 'mname', 'gender', 'adr1', 'city', 'state', 'zip', 'phone', 'dob'
+]
+
+# Iterate through each row of the NPI DataFrame
+for npi_index, npi_row in npi_with_dob.iterrows():
+    npi_key1 = npi_row['npi_key1']
+    npi_key2 = npi_row['npi_key2']
+    npi_key3 = npi_row['npi_key3']
+    npi_key4 = npi_row['npi_key4']
     
-    weighted_score = 0
+    matching_rows = pecos_with_dob[
+        (pecos_with_dob['pecos_key1'] == npi_key1) |
+        (pecos_with_dob['pecos_key2'] == npi_key2) |
+        (pecos_with_dob['pecos_key3'] == npi_key3) |
+        (pecos_with_dob['pecos_key4'] == npi_key4)
+    ]
     
-    for col in columns_to_compare:
-        npi_col = 'npi_' + col
-        pecos_col = 'pecos_' + col
-        similarity_score = calculate_similarity_score(str(npi_row[npi_col]), str(pecos_row[pecos_col]))
+    for pecos_index, pecos_row in matching_rows.iterrows():
+        weighted_score = 0
         
-        # Adjust weights as needed
-        if col in ['fname', 'lname']:
-            weighted_score += similarity_score * 25
-        elif col == 'mname':
-            weighted_score += similarity_score * 10
-        elif col == 'gender':
-            weighted_score += similarity_score * 10
-        elif col == 'adr1':
-            weighted_score += similarity_score * 25
-        elif col == 'city':
-            weighted_score += similarity_score * 10
-        elif col == 'state':
-            weighted_score += similarity_score * 15
-        elif col == 'zip':
-            weighted_score += similarity_score * 15
-        elif col == 'dob':
-            weighted_score += similarity_score * 35
-        elif col == 'phone':
-            weighted_score += similarity_score * 20  # Adjust the weight as needed
-    
-    if weighted_score > 80:
-        candidates.append({'npi_row_index': npi_row_index, 'pecos_row_index': pecos_row_index, 'weighted_score': weighted_score})
+        for col in columns_to_compare:
+            npi_col = 'npi_' + col
+            pecos_col = 'pecos_' + col
+            similarity_score = calculate_similarity_score(str(npi_row[npi_col]), str(pecos_row[pecos_col]))
+            
+            if col in ['fname', 'lname']:
+                weighted_score += similarity_score * 25
+            elif col == 'mname':
+                weighted_score += similarity_score * 10
+            elif col == 'gender':
+                weighted_score += similarity_score * 10
+            elif col == 'adr1':
+                weighted_score += similarity_score * 25
+            elif col == 'city':
+                weighted_score += similarity_score * 10
+            elif col == 'state':
+                weighted_score += similarity_score * 15
+            elif col == 'zip':
+                weighted_score += similarity_score * 15
+            elif col == 'dob':
+                weighted_score += similarity_score * 35
+            elif col == 'phone':
+                weighted_score += similarity_score * 20  # Adjust the weight as needed
+        
+        if weighted_score > 80:
+            candidates.append({'npi_row_index': npi_index, 'pecos_row_index': pecos_index, 'weighted_score': weighted_score})
 
 # Create a DataFrame from the candidates list
 candidates_df = pd.DataFrame(candidates)
@@ -69,15 +102,14 @@ candidates_df = pd.DataFrame(candidates)
 # Sort the DataFrame based on the weighted scores
 candidates_df.sort_values(by='weighted_score', ascending=False, inplace=True)
 
-# Group and store the sorted indices in a list for each row
+# Group and store the sorted indices in a list for each NPI row
 grouped = candidates_df.groupby('npi_row_index')['pecos_row_index'].apply(list)
 
 # Convert the grouped indices to a new DataFrame
 sorted_indices_df = grouped.reset_index(name='pecos_row_indices')
 
 # Export the DataFrame to a CSV file
-output_csv_path = '/path/to/sorted_indices_with_weighted_scores_updated.csv'
+output_csv_path = '/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/sorted_indices_with_weighted_scores_updated.csv'
 sorted_indices_df.to_csv(output_csv_path, index=False)
 
 print(f"New CSV file '{output_csv_path}' has been created with updated sorted indices and weighted scores.")
-
