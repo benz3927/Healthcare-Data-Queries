2bf1f756ce109e950aaecde06268aa2e4b76deb8
benz3927 <benz3927@gmail.com>
2023-08-16 10:48:48 -0400

    Update error_analysis.py

--- error_analysis.py
+++ error_analysis.py
@@ -8,7 +8,7 @@ npi_with_dob = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Quer
 pecos_with_dob = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/pecos_sample.csv', dtype=str)
 
 # Load the file with the matches you found
-sorted_indices_df = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/sorted_indices_with_weighted_scores.csv')
+sorted_indices_df = pd.read_csv('/Users/benzhao/Documents/GitHub/Healthcare-Data-Queries/data/sorted_indices_with_weighted_scores_updated.csv')
 
 # Define the list of columns to compare
 columns_to_compare = ['fname', 'lname', 'mname', 'gender', 'adr1', 'city', 'state', 'zip', 'dob', 'phone']
